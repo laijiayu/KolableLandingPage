9d8f5a95271d2ed8f10ca26aaad4bf7008df0e3a
Allison <theallisonlai@fakemail.com>
2023-03-05 20:46:42 +0800

    nav/hero layout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,42 +1,70 @@
 import React from "react"
-import "./components/App.scss"
+import "./components/App.css"
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <img src="" alt="logo" />
-        <div>
-          <ul>HOME</ul>
-          <ul>ABOUT</ul>
-          <button>
-            {" "}
-            <a href="https://make.kolable.com/contact"> GET IN TOUCH </a>
-          </button>
+      <nav className="nav-wrapper">
+        <div className="logo-img">
+          <img
+            src="https://d33wubrfki0l68.cloudfront.net/0532c6f9e5b5d8fdc8b1c4234a448603ef9aa1c6/8b0c8/img/logo.svg"
+            alt="logo"
+          />
+        </div>
+        <div className="nav-content">
+          <div className="nav-menu">
+            <ul>
+              <span>
+                <ol>HOME</ol>
+              </span>
+              <span>
+                <ol>ABOUT</ol>
+              </span>
+            </ul>
+          </div>{" "}
+          <a href="https://make.kolable.com/contact">
+            <span>GET IN TOUCH</span>
+          </a>
         </div>
       </nav>
-      <div>
-        <div>
-          <div>
-            <h1>
-              Growing your knowledge business with KOLABLE has never been easier
-            </h1>
-            <p>For influencers, consultants, educational institutions</p>
-            <p>
-              Online courses, e-commerce, O2O events ... whatever you need all
-              in one place
-            </p>
+      <div className="hero-01">
+        <div className="hero-01-content">
+          <div className="hero-01-text-content">
+            <div>
+              <h1>
+                <span>
+                  Growing your knowledge business with KOLABLE has never been
+                  easier
+                </span>
+              </h1>
+              <p>For influencers, consultants, educational institutions</p>
+              <p>
+                Online courses, e-commerce, O2O events ... whatever you need all
+                in one place
+              </p>
+              <p className="hero-01-text-content-child"></p>
+            </div>
+            <div className="hero-01-text-btn">
+              <a href="https://make.kolable.com/contact">
+                <span>GEI IN TOUCH</span>
+              </a>
+            </div>
           </div>
-          <p></p>
-          <div>
-            <button>GET IN TOUCH</button>
+          <div className="hero-01-img">
+            <img
+              src="https://make.kolable.com/hubfs/%E5%AE%98%E7%B6%B2%E5%9C%9604-1-1.png"
+              alt=""
+            />
           </div>
         </div>
       </div>
-      <img
-        src="https://make.kolable.com/hubfs/%E5%AE%98%E7%B6%B2%E5%9C%9604-1-1.png"
-        alt="官網圖"
-      />
+      <div className="recommended-wrapper">
+        <div>
+          <h3>
+            <span>Widely recommended by</span>
+          </h3>
+        </div>
+      </div>
     </div>
   )
 }
